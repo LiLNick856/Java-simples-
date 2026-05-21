2880bc2d2d24ec421f5b9f97f3c576e60f3537e5
Nicolas <nicolashian5@gmail.com>
2026-05-21 16:17:22 -0300

    Programa em java hello mais o nome

---
+++ src/ola.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class ola {
+    public static void main(String[] args) {
+        String nome = "Nicolas";
+        //variavel para o nome
+        //Mostramos a mensagem na tela juntando o "Olá, " com a sua variável
+        System.out.println("Hello " + nome + "!" );
+    }
+}
